0b12f147f3bfd6814872ee9e05cb0ea1d1f2c80f
Jeremy Meyer <jjman505@gmail.com>
2016-03-21 03:07:54 -0400

    update route for locate_shelter

--- routes/index.js
+++ routes/index.js
@@ -76,8 +76,52 @@ router.get('/campaign', function(req, res) {
 });
 
 router.get('/locate_shelter', function(req, res) {
+  var data, isProfileAdmin, shelters;
+  // TODO: Get shelter by id
+  // TODO: Get logged in user
+  // TODO: See if logged in user is admin of this shelter
+  // TODO: Get news feed for shelter
+
+  // TODO: Temporary fixture for testing
+  shelters = [
+    {
+      _id: 123,
+      avatarUrl: 'http://www.lthc.net/wp-content/themes/lthc/images/logo.png',
+      name: 'Lafayette Transitional Housing Center',
+      phone: '765-423-4880',
+      website: 'http://www.lthc.net/',
+      address: '123 Main Street\nLafayette, IN 47909',
+      bio: 'Lafayette Transitional Housing Center, Inc. is a non-profit organization which began in 1989 to develop housing, offer supportive services, and other opportunities to foster self-sufficiency for the homeless, particularly families with children, in our community.',
+      twitterUrl: 'https://twitter.com/LafTransHsg',
+      facebookUrl: 'https://www.facebook.com/Lafayette-Transitional-Housing-Center-Inc-114590221904879/',
+      email: 'lthc@lthc.net',
+      donateUrl: 'http://www.lthc.net/#homecontent'
+    },
+    {
+      _id: 456,
+      avatarUrl: 'http://www.lthc.net/wp-content/themes/lthc/images/logo.png',
+      name: 'Some other place',
+      phone: '123-456-7890',
+      website: 'http://www.lthc.net/',
+      address: '123 Main Street\nLafayette, IN 47909',
+      bio: 'Lafayette Transitional Housing Center, Inc. is a non-profit organization which began in 1989 to develop housing, offer supportive services, and other opportunities to foster self-sufficiency for the homeless, particularly families with children, in our community.',
+      twitterUrl: 'https://twitter.com/LafTransHsg',
+      facebookUrl: 'https://www.facebook.com/Lafayette-Transitional-Housing-Center-Inc-114590221904879/',
+      email: 'lthc@lthc.net',
+      donateUrl: 'http://www.lthc.net/#homecontent'
+    }
+  ];
+
+  data = {
+    results: shelters
+  };
+
+  console.log('data: ', data);
+
   res.render('locate_shelter', {
-    title: 'Locate Shelter'
+    title: 'Locate Shelter',
+    // cssFilename: 'locate_shelter',
+    data: data
   });
 });
 
