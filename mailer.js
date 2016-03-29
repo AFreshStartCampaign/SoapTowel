eccec4c0571756f4be73eed3404b319cf039b8d3
Andy Shi <shi110@purdue.edu>
2016-03-29 16:13:16 -0400

    adding mail script, transport will become ENV

---
+++ mailer.js
@@ -0,0 +1,17 @@
+module.exports.sendMail = function (req) {
+  var nodemailer = require('nodemailer');
+  var mailOptions = {
+    from: '"A Fresh Start" <afs.soaptowel@gmail.com>',
+    to: 'afs.soaptowel@gmail.com',
+    subject: "Contact Us - from " + req.body.name  + " - " + req.body.email,
+    html: "<b>" + req.body.subject + "</b><br>" + req.body.message
+  };
+
+  // send mail with defined transport object 
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      return console.log(error);
+    }
+    return console.log('Message sent: ' + info.response);
+  });
+};
