b4f8c154e939cabc5ddcbf8f57fea45e562c8011
Andy Shi <shi110@purdue.edu>
2016-03-29 16:19:29 -0400

    TRANSPORT_URL is now an env

--- mailer.js
+++ mailer.js
@@ -1,12 +1,13 @@
 module.exports.sendMail = function (req) {
   var nodemailer = require('nodemailer');
+  var transporter = nodemailer.createTransport(process.env.TRANSPORT_URL);
   var mailOptions = {
     from: '"A Fresh Start" <afs.soaptowel@gmail.com>',
     to: 'afs.soaptowel@gmail.com',
     subject: "Contact Us - from " + req.body.name  + " - " + req.body.email,
     html: "<b>" + req.body.subject + "</b><br>" + req.body.message
   };
-
+  console.log(process.env.TRANSPORT_URL);
   // send mail with defined transport object 
   transporter.sendMail(mailOptions, function(error, info){
     if(error){
